0fee2636381405ea4526a147f2c552ae8d772027
treeswift <treeswift@mailbox.org>
2023-02-07 13:42:47 -0800

    a stab at groups

--- src/grp.cpp
+++ src/grp.cpp
@@ -11,24 +11,46 @@
 #include <lm.h>       // backend
 #include <errno.h>    // error codes
 
+#include <vector>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-// group fields in struct group:
-//
-// -- preferring NetGroupGetInfo (up to level 2) to NetLocalGroupGetInfo (up to level 1)
-// char *gr_name; /* group name */
-// char *gr_passwd; /* group password */
-// gid_t gr_gid; /* group id */ 
-// -- GROUP_INFO_2 contains name and gid, ditto NET_DISPLAY_GROUP
-// -- level 3 is supported since XP
-// char **gr_mem; /* group members */ // access via: NetGroupGetUsers or NetLocalGroupGetMembers
-// -- LOCALGROUP_MEMBERS_INFO_1 conveniently includes both SID and name (_2 includes domain)
-// output freed by NetApiBufferFree
-
 namespace {
-static thread_local int curr_group;
+using namespace wusers_impl;
+
+constexpr const char* ASTER = "*"; // no such thing as a group password on Windows
+
+using GROUP_INFO_X = GROUP_INFO_2;
+constexpr int LVL = 2;
+#define GRPI(name) grpi2_##name
+
+// no heuristics and/or second guesses here, unlike FillFrom() in pwd.cpp.
+// getting the member list requires a catch-up call to NetGroupGetUsers();
+// storing it requires passing a raw memory range into `writer`. therefore
+// `writer` can't be std::function anymore. making it a virtual class.
+bool FillFrom(struct group& grp, const GROUP_INFO_X& wg_infoX, const OutWriter& writer) {
+    // all we need is:
+    grp.gr_name = writer(wg_infoX.GRPI(name));
+    grp.gr_passwd = const_cast<char*>(ASTER);
+    grp.gr_gid = wg_infoX.GRPI(group_id);
+    
+    // typically, dynamic information is represented in string format;
+    // the member list, however, is a null-terminated array of pointers.
+    // we represent this special case as a string containing raw data.
+    // don't be surprised if you see supposedly "garbage" text at the end
+    // of `out_buf` (reentrant API) or in the terminal OutBinder records
+    // (non-reentrant API).
+    // TODO std::vector<char*> mem_name_ptrs;
+
+    //writer.
+
+    // char **gr_mem;  // member list
+
+    return grp.gr_name && !errno;
+}
+
 }
 
 struct group *getgrgid(gid_t) {
@@ -68,8 +90,8 @@ int getgrnam_r(const char *, struct group *, char *, size_t, struct group **) {
 
 #if __BSD_VISIBLE
 int setgroupent(int) {
-    //
-    return 0;
+    setgrent();
+    return !errno;
 }
 
 int gid_from_group(const char *, gid_t *) {
